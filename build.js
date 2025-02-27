8c9e9abd28947f7cd1786b254325a736e88b8561
Hayk <hayk.ag942@gmail.com>
2025-02-27 16:29:58 +0400

    fix re running

--- build.js
+++ build.js
@@ -4,8 +4,11 @@ const fs = require('fs');
 const inputFile = 'index.mjml';
 const outputFile = 'index.html';
 
+console.log('Compiling MJML...');
+
 const mjmlContent = fs.readFileSync(inputFile, 'utf8');
 const htmlOutput = mjml(mjmlContent).html;
 
+// Overwrite or create new index.html with the updated content
 fs.writeFileSync(outputFile, htmlOutput);
 console.log('✅ MJML compiled to HTML successfully!');
